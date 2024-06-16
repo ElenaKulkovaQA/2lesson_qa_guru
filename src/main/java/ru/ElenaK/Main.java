82a3439e610d45d590f9e8d1c020aa8a61acf074
Elena <elena.regiionh@gmail.com>
2024-06-16 21:23:25 +0300

    change text to main

--- src/main/java/ru/ElenaK/Main.java
+++ src/main/java/ru/ElenaK/Main.java
@@ -2,7 +2,7 @@ package ru.ElenaK;
 
 public class Main {
     public static void main(String[] args) {
-        String toBePrint = "Hello, world!";
+        String toBePrint = "Hello, world and you!!!";
         // тип данных строка(String) - название переменной(toBePrint) =(присвоить) текст ("Hello world!")
 
         System.out.println(toBePrint);// выведи на печать переменную toBePrint
